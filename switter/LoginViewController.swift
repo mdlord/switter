df8b2caebf0025110ff875f0e4416ca03baa26e8
mdlord <mayankdaswani123@yahoo.com>
2017-01-12 17:30:15 -0500

    mods to login and signup UI + code

--- switter/LoginViewController.swift
+++ switter/LoginViewController.swift
@@ -2,94 +2,83 @@
 //  LoginViewController.swift
 //  switter
 //
-//  Created by Mayank Daswani on 1/11/17.
+//  Created by Mayank Daswani on 1/12/17.
 //  Copyright © 2017 Mayank Daswani. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
-class LoginViewController: UITableViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate  {
 
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        // Creating Tap Gesture to dismiss Keyboard
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        tapGesture.numberOfTapsRequired = 1
+        view.addGestureRecognizer(tapGesture)
+        
+        // Creating Swipe Gesture to dismiss Keyboard
+        let swipDown = UISwipeGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        swipDown.direction = .down
+        view.addGestureRecognizer(swipDown)
+        
+  
     }
-
+    
+    // Dismissing the Keyboard with the Return Keyboard Button
+    func dismissKeyboard(gesture: UIGestureRecognizer){
+        self.view.endEditing(true)
+    }
+    
+    // Dismissing the Keyboard with the Return Keyboard Button
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        usernameTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        return true
+    }
+    
+    // Moving the View down after the Keyboard appears
+    func textFieldDidBeginEditing(textField: UITextField) {
+        animateView(up: true, moveValue: 80)
+    }
+    
+    // Moving the View down after the Keyboard disappears
+    func textFieldDidEndEditing(textField: UITextField) {
+        animateView(up: false, moveValue: 80)
+    }
+    
+    
+    // Move the View Up & Down when the Keyboard appears
+    func animateView(up: Bool, moveValue: CGFloat){
+        
+        let movementDuration: TimeInterval = 0.3
+        let movement: CGFloat = (up ? -moveValue : moveValue)
+        UIView.beginAnimations("animateView", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(movementDuration)
+        self.view.frame = self.view.frame.offsetBy(dx: 0, dy: movement)
+        UIView.commitAnimations()
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    
+    
+    //user login
+    @IBAction func loginAction(_ sender: Any) {
 
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
